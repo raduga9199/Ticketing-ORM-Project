c7782350366706f2f6c58d9846021d502469c718
raduga9199 <raduga9199@gmail.com>
2023-07-26 09:06:47 -0400

    Security - p3

---
+++ src/main/java/com/cydeo/service/SecurityService.java
@@ -0,0 +1,9 @@
+package com.cydeo.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface SecurityService extends UserDetailsService {
+
+}
